8794261eff6dcfc73b002f7e13c7a7a44f04545b
Diego Lopes <md2diego@Diegos-MacBook-Air.local>
2026-05-26 19:37:40 -0300

    try fix env

--- src/Database.php
+++ src/Database.php
@@ -9,11 +9,12 @@ class Database
 
     private function __construct()
     {
-        $client = new Client(getenv('MONGODB_URI') ?? $_ENV['MONGODB_URI']);
+        var_dump(getenv());
+        $client = new Client(getenv('MONGODB_URI') ?: $_ENV['MONGODB_URI']);
         // For MongoDB Atlas:
         // $client = new Client("mongodb+srv://<user>:<pass>@cluster.mongodb.net");
 
-        $this->db = $client->selectDatabase(getenv('DB') ?? $_ENV['DB']);
+        $this->db = $client->selectDatabase(getenv('DB') ?: $_ENV['DB']);
     }
 
     public static function getInstance(): self
